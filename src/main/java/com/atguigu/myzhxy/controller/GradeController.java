8695b2b2369845c07bdfe16a36994630cdd96063
luo <284257802@qq.com>
2024-02-22 09:34:14 +0800

    增加年级管理带条件分页查询功能

--- src/main/java/com/atguigu/myzhxy/controller/GradeController.java
+++ src/main/java/com/atguigu/myzhxy/controller/GradeController.java
@@ -1,9 +1,27 @@
 package com.atguigu.myzhxy.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.atguigu.myzhxy.pojo.Grade;
+import com.atguigu.myzhxy.service.GradeService;
+import com.atguigu.myzhxy.util.Result;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController//因为是异步交互，返回数据类型的方法，防止@ResponseBody的重复使用
 @RequestMapping("/sms/gradeController")//请求的运输路径，sms为本地controller层上
 public class GradeController {
+    @Autowired
+    private GradeService gradeService;
+
+    @GetMapping("/getGrades/{pageNo}/{pageSize}")
+    //本项目中返回的一切Jason对象都用result来获取，需要获得单独某个属性就放入result中
+    public Result getGrades(@PathVariable Integer pageNo, @PathVariable Integer pageSize,@RequestParam("gradeName") String gradename){
+        //分页带条件查询
+        Page<Grade> page = new Page<>(pageNo,pageSize);
+        //通过服务层来查询
+        IPage<Grade> pageRs = gradeService.getGradeByOpr(page,gradename);
+        //封装result对象返回
+        return Result.ok(pageRs);
+    }
 }
--- src/main/java/com/atguigu/myzhxy/service/impl/GradeServiceImpl.java
+++ src/main/java/com/atguigu/myzhxy/service/impl/GradeServiceImpl.java
@@ -3,11 +3,28 @@ package com.atguigu.myzhxy.service.impl;
 import com.atguigu.myzhxy.mapper.GradeMapper;
 import com.atguigu.myzhxy.pojo.Grade;
 import com.atguigu.myzhxy.service.GradeService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import freemarker.template.utility.StringUtil;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
 
 @Service("gradeServiceImpl")
 @Transactional
 public class GradeServiceImpl extends ServiceImpl<GradeMapper, Grade> implements GradeService {
+    @Override
+    public IPage<Grade> getGradeByOpr(Page<Grade> pageParam, String gradename) {
+        QueryWrapper queryWrapper = new QueryWrapper();//使用querywrapper来进行查询
+        //判断查询是否为空
+        if (!StringUtils.isEmpty(gradename)){
+            queryWrapper.like("name",gradename);
+        }
+        //根据ID降序排序
+        queryWrapper.orderByDesc("id");
+        Page<Grade> page = baseMapper.selectPage(pageParam, queryWrapper);
+        return page;
+    }
 }
