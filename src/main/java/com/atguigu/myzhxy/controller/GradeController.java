3d4c0c4829037ddbd0e0e68aafde5cb64250f791
QiQi Luo <284257802@qq.com>
2024-02-26 17:25:32 +0800

    增加年级删除功能

--- src/main/java/com/atguigu/myzhxy/controller/GradeController.java
+++ src/main/java/com/atguigu/myzhxy/controller/GradeController.java
@@ -8,11 +8,20 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController//因为是异步交互，返回数据类型的方法，防止@ResponseBody的重复使用
 @RequestMapping("/sms/gradeController")//请求的运输路径，sms为本地controller层上
 public class GradeController {
     @Autowired
     private GradeService gradeService;
+
+    @DeleteMapping("/deleteGrade")
+    public Result deleteGrade(@RequestBody List<Integer> ids){
+
+        gradeService.removeByIds(ids);
+        return Result.ok();
+    }
     @PostMapping("saveOrUpdateGrade")//增加功能
     public Result saveOrUpdateGrade(@RequestBody Grade grade){//因为请求的返回的是JASON格式，现在转换成grade来接受
         //接受参数
