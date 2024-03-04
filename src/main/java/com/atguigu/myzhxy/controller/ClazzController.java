48cea7f1f65cf666761f6cfcaac1478cca6c2144
QiQi Luo <284257802@qq.com>
2024-03-04 18:05:42 +0800

    增加年级管理删除功能

--- src/main/java/com/atguigu/myzhxy/controller/ClazzController.java
+++ src/main/java/com/atguigu/myzhxy/controller/ClazzController.java
@@ -11,13 +11,26 @@ import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Api("班级管理器")
 @RestController//因为是异步交互，返回数据类型的方法，防止@ResponseBody的重复使用
 @RequestMapping("/sms/clazzController")//请求的运输路径，sms为本地controller层上
 public class ClazzController {
 
+
     @Autowired
     private ClazzService clazzService;
+
+    //sms/clazzController/deleteClazz
+    @ApiOperation("删除班级管理")
+    @DeleteMapping("/deleteClazz")
+    public Result deleteClazz(@ApiParam("要删除的班级id的json集合")@RequestBody List<Integer> ids){
+
+        clazzService.removeByIds(ids);
+        return Result.ok();
+
+    }
 
     @ApiOperation("增加或者修改班级信息")
     @PostMapping("/saveOrUpdateClazz")
